a73e3763fcc8666ae859f3f01981cc72ca8a7c23
Junying Chen <jygchen@gmail.com>
2025-11-04 09:33:18 +1100

    simplify

--- MyDogsbody.Logging/LoggingContext.fs
+++ MyDogsbody.Logging/LoggingContext.fs
@@ -6,14 +6,10 @@ open LiteDB
 
 let getLoggingDatabaseContext databasePath connectionType: LoggingDatabaseContext =
     let exceptionCollectionName = "Exceptions"
-    try
-        let dbConnection =
-            new LiteDatabase($"Filename={databasePath};connection={connectionType}")
-        let getExceptionCollection() =
-            dbConnection.GetCollection<ExceptionLog>(exceptionCollectionName)
-        {
-            GetExceptionCollection = getExceptionCollection
-        }
-    with ex ->
-        printfn "Failed to create LiteDB connection: %s" ex.Message
-        reraise()
+    let dbConnection =
+        new LiteDatabase($"Filename={databasePath};connection={connectionType}")
+    let getExceptionCollection() =
+        dbConnection.GetCollection<ExceptionLog>(exceptionCollectionName)
+    {
+        GetExceptionCollection = getExceptionCollection
+    }
